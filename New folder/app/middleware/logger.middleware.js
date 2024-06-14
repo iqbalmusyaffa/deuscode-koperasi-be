62fac19bb4c3075b24d2ee189da3bd2fcd9826c8
iqbalmusyaffa <yusa82401@gmail.com>
2024-06-14 19:39:03 +0700

    update hilang new folder

--- New folder/app/middleware/logger.middleware.js
+++
@@ -1,9 +0,0 @@
-const logger = require("../logger.js");
-
-const loggerMiddleware = (req, res, next) => {
-  const user = req.user;
-  logger.info(`${req.method} ${req.url}`, { user });
-  next();
-};
-
-module.exports = loggerMiddleware;
